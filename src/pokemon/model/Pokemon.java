36a92c5b81c22a3d93a2451eb7c23d13a02abd98
inperium <ibow9416@740s-pl-111.local>
2016-12-05 12:53:53 -0700

    Working on Pokemon

--- src/pokemon/model/Pokemon.java
+++ src/pokemon/model/Pokemon.java
@@ -2,5 +2,37 @@ package pokemon.model;
 
 public class Pokemon
 {
-
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
+	public String getPokemonTypes()
+	{
+		String pokemonTypes = "This Pokemon is of type:\n";
+		Class<?> [] types = getClass().getInterfaces();
+		String [] pokeTypes = new String[types.length];
+		for(int index = 0; index < types.length; index++)
+		{
+			String temp = types[index].getName();
+			
+			pokeTypes[index] = temp;
+		}
+		
+		for(String current : pokeTypes)
+		{
+			String temp = current.replace(this.getclass().getPackage().getName() + ".", "");
+			pokemonTypes += temp = "\n";
+		}
+		
+		return pokemonTypes;
+	}
+	*/
 }
