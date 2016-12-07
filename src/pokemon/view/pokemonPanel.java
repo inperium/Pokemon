f2a97ea9cb9e22ba0f9c0af9474345de5b25f85a
inperium <ibow9416@740s-pl-111.local>
2016-12-07 13:01:26 -0700

    Fixed test error on line 39.

--- src/pokemon/view/pokemonPanel.java
+++ src/pokemon/view/pokemonPanel.java
@@ -2,19 +2,60 @@ package pokemon.view;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+
 import pokemon.controller.PokemonController;
 
-public class PokemonPanel
+public class pokemonPanel
 {
 	private JButton updateButton;
 	private PokemonController baseController;
+	private SpringLayout Layout;
+	private ImageIcon pokemonIcon;
+	private JTextField nameField;
+	private JTextField combatField;
+	private JTextField healthField;
+	private JTextField speedField;
+	private JTextField numberField;
+	private JLabel advancedLabel;
+	private JTextArea advancedArea;
+	private JComboBox pokedexSelector;
+	private JLabel combatLabel;
+	private JLabel healthLabel;
+	private JLabel speedLabel;
+	private JLabel numberLabel;
+	private JLabel nameLabel;
+	private JLabel pokemonLabel;
 	
-	public void Pokemon(PokemonController baseController)
+	public void PokemonPanel(PokemonController baseController)
 	{
-		super();
 		this.baseController = baseController;
+		this.Layout= new SpringLayout();
+		this.pokemonIcon = new ImageIcon(getClass().getResource("/pokemon/view/images/pokeball.jpg"));
+		this.updateButton = new JButton("Udate stats!");
+		this.nameField = new JTextField(25);
+		this.combatField = new JTextField(5);
+		this.healthField = new JTextField(5);
+		this.speedField = new JTextField(5);
+		this.numberField = new JTextField(5);
+		this.advancedArea = new JTextArea(10, 25);
+		this.pokedexSelector = new JComboBox(baseController.buildPokedexText());
+		this.advancedLabel = new JLabel("Advanced information");
+		this.combatLabel = new JLabel("Combat Points: ");
+		this.healthLabel = new JLabel("Health Points: ");
+		this.speedLabel = new JLabel("The Pokemon's speed is ");
+		this.numberLabel = new JLabel("The Pokemon's number is ");
+		this.nameLabel = new JLabel("The Pokemon's name is ");
+		this.pokemonLabel = new JLabel("The Current pokemon", pokemonIcon,JLabel.CENTER);
+		
+
 		
 		setupPanel();
 		setupLayout();
@@ -38,6 +79,7 @@ public class PokemonPanel
 	
 	private void setupListeners()
 	{
+		/*
 		pokedexSelector.addActionListner(new ActionListener()
 		{
 			public void actionPerformed(ActionEvent selection)
@@ -65,7 +107,8 @@ public class PokemonPanel
 					baseController.getPokedex().get(selected).setAttackPoints(Integer.parseInt(combatField.getText()));
 					baseController.getPokedex().get(selected).setSpeed(Double.parseDouble(speedField.getText()));
 				}
-			}
+			});
 		}
+		*/
 	}
 }
